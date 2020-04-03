382c70c6a8f2d7f55dc40a50485933b9de2c7be3
Juan Dausa <juandausa@gmail.com>
2020-04-03 09:21:55 -0300

    Se agrega un proyecto de tests de ejemplo

---
+++ Models.Tests/ExampleTestCase.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Models.Tests
+{
+    public class ExampleTestCase
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
